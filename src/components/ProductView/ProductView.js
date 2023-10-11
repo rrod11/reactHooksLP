18ecb508108b4a9d0d158f188012532c3dd08e51
Roderick <Roderickj800@gmail.com>
2023-10-11 18:15:52 -0400

    product view

--- src/components/ProductView/ProductView.js
+++ src/components/ProductView/ProductView.js
@@ -1,35 +1,48 @@
-import React from 'react';
+import React, { useEffect } from "react";
 import ProductListItem from "../ProductListItem";
 import ProductDetails from "../ProductDetails";
-import './ProductView.css'
-
+import "./ProductView.css";
+import { useState } from "react";
 function ProductView({ products }) {
-
+  const [sideOpen, setSideOpen] = useState(true);
+  const [selectedProduct, setSelectedProduct] = useState("");
   // TODO: Replace with state variable
-  const sideOpen = true;
-
+  // const sideOpen = true;
+  useEffect(() => {
+    if (selectedProduct) {
+      setSideOpen(true);
+    }
+  }, [selectedProduct]);
+  useEffect(() => {
+    if (!sideOpen) {
+      setSelectedProduct();
+    }
+  }, [sideOpen]);
   return (
     <div className="product-view">
       <div className="product-main-area">
         <h1>Products</h1>
         <div className="product-list">
-          {products.map(item =>
+          {products.map((item) => (
             <ProductListItem
               key={item.id}
               product={item}
-              onClick={() => console.log('SELECT PRODUCT', item)}
+              isSelected={selectedProduct?.id === item.id}
+              onClick={() => setSelectedProduct(item)}
             />
-          )}
+          ))}
         </div>
       </div>
       <div className="product-side-panel">
         <div className="product-side-panel-toggle-wrapper">
-          <div className="product-side-panel-toggle"
-               onClick={() => console.log('TOGGLE SIDE PANEL')}>
-            {sideOpen ? '>' : '<'}
+          <div
+            className="product-side-panel-toggle"
+            onClick={() => setSideOpen(!sideOpen)}
+          >
+            {sideOpen ? ">" : "<"}
           </div>
         </div>
-        <ProductDetails visible={sideOpen} />
+        <ProductDetails visible={sideOpen} product={selectedProduct} />
       </div>
     </div>
   );
